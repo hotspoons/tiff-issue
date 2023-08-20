4113a8b481d4188cd70155f4ebbf19fa716f7f6d
Richard Siomporas <richard.siomporas@gmail.com>
2023-08-20 11:51:24 -0400

    Refactored to remove rescaling since that wasn't a core concern

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,14 @@
 use std::{fs, env};
 use std::io::{Cursor, BufWriter};
-use std::num::NonZeroU32;
-use std::time::SystemTime;
-use fax::{VecWriter, decoder, decoder::pels, BitWriter, Bits, Color};
-
-use image::{ImageError, ImageEncoder, ColorType, DynamicImage, GrayImage};
-use image::codecs::png::PngEncoder;
+use fax::{decoder, decoder::pels, Color};
+use image::{ImageError, DynamicImage, GrayImage};
 use image::io::Reader as ImageReader;
-use fast_image_resize as fr;
 
-
-pub struct Resize{
-    pub w: i32,
-    pub h: i32
-}
-
-fn resize_dyn_img_to_png_bytes(img: DynamicImage, framing: &Resize) -> Result<Vec<u8>, ImageError> {
-    let now: SystemTime = SystemTime::now();
+fn resize_dyn_img_to_png_bytes(dyn_image: DynamicImage) -> Result<Vec<u8>, ImageError> {
     let mut image_data = BufWriter::new(Cursor::new(Vec::new()));
-            
-    if framing.h > 0 || framing.w > 0{
-        let mut h = framing.h as u32;
-        let mut w = framing.w as u32;
-        if framing.h <= 0{
-            h = (((img.height() as f32) / (img.width() as f32)) * framing.w as f32).ceil() as u32;
-        }
-        else if framing.w <= 0{
-            w = (((img.width() as f32) / (img.height() as f32)) * framing.h as f32).ceil() as u32;
-        }
-        let mut src_image = fr::Image::from_vec_u8(
-            NonZeroU32::new(img.width()).unwrap(),
-            NonZeroU32::new(img.height()).unwrap(),
-            img.to_rgba8().into_raw(),
-            fr::PixelType::U8x4,
-        ).unwrap();
-        let alpha_mul_div = fr::MulDiv::default();
-        alpha_mul_div
-            .multiply_alpha_inplace(&mut src_image.view_mut())
-            .unwrap();
-        let mut dst_image = fr::Image::new(
-            NonZeroU32::new(w).unwrap(),
-            NonZeroU32::new(h).unwrap(),
-            src_image.pixel_type(),
-        );
-        let mut dst_view = dst_image.view_mut();
-        let mut resizer = fr::Resizer::new(
-            fr::ResizeAlg::Convolution(fr::FilterType::Lanczos3),
-        );
-        resizer.resize(&src_image.view(), &mut dst_view).unwrap();
-        // Divide RGB channels of destination image by alpha
-        alpha_mul_div.divide_alpha_inplace(&mut dst_view).unwrap();
-        PngEncoder::new(&mut image_data).write_image(
-            dst_image.buffer(),
-            w,
-            h,
-            ColorType::Rgba8,
-        ).unwrap();
-        
-    }
-    else{
-        img.write_to(&mut image_data, image::ImageOutputFormat::Png).unwrap();
-    }
-    match now.elapsed() {
-        Ok(elapsed) => {
-            println!("{} ms (img conversion and scaling time)", elapsed.as_millis());
-        }
-        Err(e) => {
-            println!("Error: {e:?}");
-        }
-    }
-    let response = image_data.get_ref().get_ref().to_owned();
-    return Ok(response);
+    dyn_image.write_to(&mut image_data, image::ImageOutputFormat::Png).unwrap();
+    return Ok(image_data.get_ref().get_ref().to_owned());
 }
-
 
 fn image_preprocess_fax(bytes:Vec<u8>) -> Result<GrayImage, Box<dyn std::error::Error>> {
     use tiff::decoder::Decoder;
@@ -85,8 +21,6 @@ fn image_preprocess_fax(bytes:Vec<u8>) -> Result<GrayImage, Box<dyn std::error::
     
     let strip_offsets = decoder.get_tag_u32_vec(Tag::StripOffsets).unwrap();
     let strip_lengths = decoder.get_tag_u32_vec(Tag::StripByteCounts).unwrap();
-
-    dbg!((height, width));
 
     let mut image = GrayImage::new(width, height);
 
@@ -110,24 +44,21 @@ fn image_preprocess_fax(bytes:Vec<u8>) -> Result<GrayImage, Box<dyn std::error::
         });
     }
     
-    dbg!(cols_read);
     Ok(image)
 }
 
-pub fn read_bytes_to_png_bytes(mut bytes:Vec<u8>, framing: &Resize) -> Result<Vec<u8>, ImageError>{
-    dbg!(&bytes[..3]);
+pub fn read_bytes_to_png_bytes(bytes:Vec<u8>,) -> Result<Vec<u8>, ImageError>{
     if bytes.len() > 3 && bytes[0] == 73 && bytes[1] == 73 // big endian, fine for POC
         && bytes[2] == 42{
             let image = image_preprocess_fax(bytes).unwrap();
-            return resize_dyn_img_to_png_bytes(image.into(), framing);
-            // TODO reconstruct a tiff or other image representation using the decompressed bitmap
+            return resize_dyn_img_to_png_bytes(image.into());
         }
 
     let reader = ImageReader::new(Cursor::new(bytes)).with_guessed_format()?;
 
     match reader.decode(){
         Ok(img) => {
-          return resize_dyn_img_to_png_bytes(img, framing);
+          return resize_dyn_img_to_png_bytes(img);
         }
         Err(e) => {
             eprint!("Error decoding image: {}", e);
@@ -136,19 +67,18 @@ pub fn read_bytes_to_png_bytes(mut bytes:Vec<u8>, framing: &Resize) -> Result<Ve
     };
 }
 
-pub fn read_image_to_png_bytes(path: &String, framing: &Resize) -> Result<Vec<u8>, ImageError>{
+pub fn read_image_to_png_bytes(path: &String) -> Result<Vec<u8>, ImageError>{
     let bytes = fs::read(path).unwrap();
-    return read_bytes_to_png_bytes(bytes, framing);
+    return read_bytes_to_png_bytes(bytes);
 }
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1{
         let path = &args[1];
-        let image_bytes = read_image_to_png_bytes(path, &Resize { w: 1280, h:1650 }).unwrap();
-        println!("Image resized, {} bytes in final PNG representation", image_bytes.len());
+        let image_bytes = read_image_to_png_bytes(path).unwrap();
         std::fs::write("out.png", &image_bytes).unwrap();
+        println!("Image converted to out.png, {} bytes in final PNG representation", image_bytes.len());
     }
     else{
         println!("Usage - this_executable <path/to/input-image.any>");
